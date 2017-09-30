f6b4e41662a65423503af0ae1b73febb59342ec8
go.ugotsta.com <ugotsta@gmail.com>
2017-09-30 05:17:08 -0700

    Initial implementation roughly fleshed out.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+let context = new AudioContext()
+let analyser = context.createAnalyser()
+
+let audio = new Audio()
+audio.loop = true
+audio.crossOrigin = 'anonymous'
+
+let source = context.createMediaElementSource(audio)
+source.connect(analyser)
+analyser.connect(context.destination)
+
+var bands = 16;
+var range = 750/bands;
+var current_band = 1;
+var current_range = range;
+var current_average = [];
+
 jQuery(document).ready(function() {
     
     var toggle_html='<span class="toggle"></span>';
@@ -10,123 +27,114 @@ jQuery(document).ready(function() {
     var $gd = $('#wrapper').data('gitdown');
 
     function main() {
-        
-        // add choice class to section li links
-        $('.section .content a[href*="#"]').addClass('choice');
-        
-        register_events();
-        update();
-        find_video_references();
-    }
-    
-    function find_video_references() {
-        $('.section a img').each(function(){
-            var alt = $(this).attr('alt');
-            //console.log(alt);
-            if ( alt === 'inner-background') {
-                var url = $(this).parent().attr('href');
-                var id = '';
-                if(url.match('http://(www.)?youtube|youtu\.be')){
-                    id = url.split(/v\/|v=|youtu\.be\//)[1].split(/[?&]/)[0];
-                }
-                var iframe = '<iframe id="player" class="video-inner muted" src="https://www.youtube.com/embed/' + id + '?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=' + id  + '" frameborder="0" allowfullscreen></iframe>';
-                $('.inner').append(iframe);
-            }
-            // now remove original link
-            $(this).parent().remove();
-        });
-    }
-    
-    function update() {
-        var current = $('.section.current').attr('id');
-        // update window title
-        document.title = $('.section.current a.handle').text();
-        
-        // update browser variable with progress
-        if ( progress.indexOf(current) === -1 ) progress.push(current);
-        window.localStorage.setItem( 'entwine_progress', progress.join(",") );
-        
-        // hide all toc links
-        $('.toc a').hide();
-        
-        // now show only those that user has progressed through
-        for ( var i = 0; i < progress.length; i++ ) {
-            var id = progress[i];
-            $('a[href^="#' + id + '"]').show();
-        }
-        
-        // add Back button if past first section
-        var header = $('.section.header').attr('id');
-        if ( current === header ) {
-            console.log('User at start.');
-            $('.back').remove();
-        } else {
-            // remove any prior Back buttons first
-            $('.back').remove();
-            // add new button
-            $('.section.current').append('<a class="back">Back</a>');
-            $( ".back" ).click(function() {
-                window.history.back();
-            });
-        }
 
-    }
-    
-    // returns an array of choices available for input section
-    function find_choices(section) {
-        var choices = [];
-        var $parent = $('.section#' + section);
-        var $choices = $parent.find('.content a.choice');
-        $choices.each(function(i, val){
-            var id = val['href'].split('#')[1];
-            choices.push(id);
-        });
-        return choices;
-    }
-    
-    // returns an array of sections that lead to input section
-    function find_sections_linking_here(section) {
-        // ex: path = ['living-quarters','great-hall','throne-room']
-        var path = [];
+        // create eq container
+        var $eq = create_eq(bands);
+        var width = $eq.width();
+        var height = $eq.height();
         
-        // for each section
-        $('.section').each(function(){
-            var id = $(this).find('a.handle').attr('name');
-            var choices = find_choices(id);
-            if ( choices.indexOf(section) != -1 ) {
-                path.push(id);
-            }
-        });
-        return path;
+        nextSound();
+        loop();
     }
-    
+
+    function create_eq(bands) {
+        var html = '<div class="eq"></div>';
+        $('.inner').append(html);
+        var $eq = $('.eq');
+        var width = $eq.width();
+        var height = $eq.height();
+
+        var spacing = 2;
+
+        var band_width = ( width / bands ) - spacing;
+        var band_height = 90;
+
+        html = '<div class="eq-inner">';
+        var x = 0;
+        for ( var i = 1; i <= bands; i++ ) {
+            html += '<div class="band band-' + i + '"';
+            html += ' style="width:' + band_width + 'px; height:' + band_height + 'px;';
+            html += ' left: ' + x + 'px;';
+            html += '">';
+            html += '</div>';
+            x += band_width + spacing;
+        }
+        html += '</div>';
+        $eq.append(html);
+        return $eq;
+    }
+        
     function register_events() {
-        
-        // handle history
-        $(window).on('popstate', function (e) {
-            update();
-        });
-        
-        // hide sections and toc reference when toggled
-        $( '.section .toggle' ).click(function() {
-            var name = $(this).parent().attr('name');
-            $( '#' + name ).hide();
-            // add hidden class to toc item;
-            $( '.toc a[href*="#' + name + '"]' ).addClass('hidden');
-        });
-        
-        // add click event to toggle items in toc
-        $( '.toc .toggle' ).click(function() {
-            var name = $(this).parent().attr('href');
-            // toggle hidden status
-            if( $(this).parent().hasClass('hidden') ) {
-                $(name).show();
-                $(this).parent().removeClass('hidden');
-            } else {
-                $(name).hide();
-                $(this).parent().addClass('hidden');
-            }
-        });
     }
+
+    function nextSound(){
+        let http = new XMLHttpRequest();
+        http.onload = () => { 
+            if(http.responseText){
+                let result = JSON.parse(http.responseText);
+                console.log('Sounds: ' + result.length);
+                let music = result[parseInt(Math.random() * result.length)];
+                
+                var author = '<span id="author">';
+                author += '<a href="#author"></a>';
+                author += '</span>';
+                $('.section.header').append(author);
+                document.querySelector('#author').hidden = false;
+                let link = document.querySelector('#author a');
+                link.text = music.title;
+                link.href = music.permalink_url;
+
+                audio.src = music.stream_url + '?client_id=ea6d4c6a6f367767516c9221a30c2ced';
+                audio.play();
+            }
+        }
+        http.open("GET", 'http://api.soundcloud.com/tracks?limit=100&genres=rock&client_id=ea6d4c6a6f367767516c9221a30c2ced', true);
+        http.send();
+    }
+
+    function loop(){
+        window.requestAnimationFrame(loop);
+        let freq = new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteFrequencyData(freq);
+
+        let data = new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteTimeDomainData(data);
+        
+        freq.forEach((f, i) => resize(f,i));
+    }
+
+    function resize( freq, index ){
+        var $band = $('.eq .band-' + current_band);
+        if ( index < current_range ) {
+            $band.height(freq * 2);
+            //current_average.push(freq);
+        } else {
+            // var avg = 0;
+            // if ( current_average.length > 0 ) {
+            //     // iterate over averages and average them
+            //     var total = 0;
+            //     for( var i = 0; i < current_average.length; i++ ) {
+            //         total += current_average[i];
+            //     }
+            //     avg = total / current_average.length;
+            // }            
+            // render height change if changed
+            
+            // if ( $band.height() != avg ) {
+            //     //console.log('current_band: ' + current_band + ' | range: ' + range);
+            //     $band.height(avg * 3);
+            // }
+            // reset current values
+            current_average = [];
+            current_band += 1;
+            current_range += range;
+            if ( current_band > bands ) {
+                // back to 1, reset to default values
+                current_band = 1;
+                current_range = range;
+            }
+        }
+    }
+
 
 });
