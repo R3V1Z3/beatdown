df610dfd65117cc498d8f1d8ed3b949f1d2b7892
go.ugotsta.com <ugotsta@gmail.com>
2017-10-01 23:51:08 -0700

    Fixed band logic.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
-let context = new AudioContext()
-let analyser = context.createAnalyser()
-
-let audio = new Audio()
-audio.loop = true
-audio.crossOrigin = 'anonymous'
-
-let source = context.createMediaElementSource(audio)
-source.connect(analyser)
-analyser.connect(context.destination)
-
 var bands = 16;
-var range = 750/bands;
-var current_band = 1;
-var current_range = range;
-var current_average = [];
+var spacing = 10;
+
+var query = 'ugotsta';
+var client_id = 'ea6d4c6a6f367767516c9221a30c2ced';
+
+// webaudio configuration
+let context = new AudioContext();
+let analyser = context.createAnalyser();
+analyser.fftSize = bands * 2;
+
+let audio = new Audio();
+audio.loop = true;
+audio.crossOrigin = 'anonymous';
+
+let source = context.createMediaElementSource(audio);
+source.connect(analyser);
+analyser.connect(context.destination);
 
 jQuery(document).ready(function() {
     
@@ -29,7 +31,7 @@ jQuery(document).ready(function() {
     function main() {
 
         // create eq container
-        var $eq = create_eq(bands);
+        var $eq = create_eq(bands, spacing);
         var width = $eq.width();
         var height = $eq.height();
         
@@ -37,21 +39,19 @@ jQuery(document).ready(function() {
         loop();
     }
 
-    function create_eq(bands) {
+    function create_eq(bands, spacing) {
         var html = '<div class="eq"></div>';
         $('.inner').append(html);
         var $eq = $('.eq');
         var width = $eq.width();
         var height = $eq.height();
 
-        var spacing = 2;
-
         var band_width = ( width / bands ) - spacing;
         var band_height = 90;
 
         html = '<div class="eq-inner">';
         var x = 0;
-        for ( var i = 1; i <= bands; i++ ) {
+        for ( var i = 0; i < bands; i++ ) {
             html += '<div class="band band-' + i + '"';
             html += ' style="width:' + band_width + 'px; height:' + band_height + 'px;';
             html += ' left: ' + x + 'px;';
@@ -72,7 +72,6 @@ jQuery(document).ready(function() {
         http.onload = () => { 
             if(http.responseText){
                 let result = JSON.parse(http.responseText);
-                console.log('Sounds: ' + result.length);
                 let music = result[parseInt(Math.random() * result.length)];
                 
                 var author = '<span id="author">';
@@ -84,11 +83,14 @@ jQuery(document).ready(function() {
                 link.text = music.title;
                 link.href = music.permalink_url;
 
-                audio.src = music.stream_url + '?client_id=ea6d4c6a6f367767516c9221a30c2ced';
+                audio.src = music.stream_url + '?client_id=' + client_id;
                 audio.play();
             }
         }
-        http.open("GET", '//api.soundcloud.com/tracks?limit=100&genres=rock&client_id=ea6d4c6a6f367767516c9221a30c2ced', true);
+        var url = '//api.soundcloud.com/tracks/?';
+        url += 'q=' + query;
+        url += '&client_id=' + client_id;
+        http.open("GET", url, true);
         http.send();
     }
 
@@ -100,40 +102,17 @@ jQuery(document).ready(function() {
         let data = new Uint8Array(analyser.frequencyBinCount);
         analyser.getByteTimeDomainData(data);
         
-        freq.forEach((f, i) => resize(f,i));
+        freq.forEach((f, i) => update_band(f,i));
     }
 
-    function resize( freq, index ){
-        var $band = $('.eq .band-' + current_band);
-        if ( index < current_range ) {
-            $band.height(freq * 2);
-            //current_average.push(freq);
-        } else {
-            // var avg = 0;
-            // if ( current_average.length > 0 ) {
-            //     // iterate over averages and average them
-            //     var total = 0;
-            //     for( var i = 0; i < current_average.length; i++ ) {
-            //         total += current_average[i];
-            //     }
-            //     avg = total / current_average.length;
-            // }            
-            // render height change if changed
-            
-            // if ( $band.height() != avg ) {
-            //     //console.log('current_band: ' + current_band + ' | range: ' + range);
-            //     $band.height(avg * 3);
-            // }
-            // reset current values
-            current_average = [];
-            current_band += 1;
-            current_range += range;
-            if ( current_band > bands ) {
-                // back to 1, reset to default values
-                current_band = 1;
-                current_range = range;
-            }
-        }
+    function update_band( freq, i ){
+        var $band = $('.eq .band-' + i);
+        $band.attr('data-range', freq);
+
+        // freq maxes at 256, scale freq to fit eq height
+        var h = $('.eq').height();
+        var f = 0 + (h - 0) * (freq - 0) / (0 - -256);
+        $band.height(f);
     }
 
 
