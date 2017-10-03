4de8e0a30f98e088c0aa68dae9e5bc13c611e03a
go.ugotsta.com <ugotsta@gmail.com>
2017-10-02 22:12:21 -0700

    Various style updates.

--- README.md
+++ README.md
@@ -2,15 +2,17 @@
 A music visualizer in HTML themed using CSS.
 
 ## What?
-Wait, why would anyone build a music visualizer using the HTML DOM? HTML and CSS offer impressive visual power these days and while not as fast as native or as HTML5 canvas, CSS facilitates customization by means that designers already know.
+Wait, why would anyone build a music visualizer with HTML? HTML and CSS offer impressive visual power these days and while not as fast as native code, CSS facilitates customization by means that designers already know.
 
 # Examples <!-- {$gd_info} -->
 <!-- {$gd_help_ribbon} -->
-### Don't just listen, see it.  
+### Hear it, See it
 
-Tracks <!-- {$gd_tracks} -->
-- Never Again
-- Nothing
+Tracks <!-- {$gd_select_music_ids} -->
+- [Amen](https://soundcloud.com/ugotsta/son-of-no-man-amen)
+- [Nothing](https://soundcloud.com/ugotsta/nothing)
+- [Saint Billy](https://soundcloud.com/ugotsta/saint-billy-version-b0-1)
+- [Insanity's Bliss](https://soundcloud.com/ugotsta/insanitys-bliss)
 
 Themes <!-- {$gd_css} -->
 
--- js/script.js
+++ js/script.js
@@ -34,7 +34,8 @@ jQuery(document).ready(function() {
         var $eq = create_eq(bands, spacing);
         var width = $eq.width();
         var height = $eq.height();
-        
+
+        register_events();        
         nextSound();
         loop();
     }
@@ -47,7 +48,7 @@ jQuery(document).ready(function() {
         var height = $eq.height();
 
         var band_width = ( width / bands ) - spacing;
-        var band_height = 90;
+        var band_height = 0;
 
         html = '<div class="eq-inner">';
         var x = 0;
@@ -63,8 +64,16 @@ jQuery(document).ready(function() {
         $eq.append(html);
         return $eq;
     }
-        
-    function register_events() {
+
+    function get_track_info(url) {        
+        let resolveUrl = `http://api.soundcloud.com/resolve.json?url=${url}/tracks&client_id=${client_id}`
+        fetch(resolveUrl, {
+            method: 'get'
+        }).then((response) => {
+            return response.json()
+        }).then((result) => {
+            return result;
+        })
     }
 
     function nextSound(){
@@ -77,19 +86,23 @@ jQuery(document).ready(function() {
                 var author = '<span id="author">';
                 author += '<a href="#author"></a>';
                 author += '</span>';
-                $('.section.header').append(author);
+                $('.section.header .content').append(author);
                 document.querySelector('#author').hidden = false;
                 let link = document.querySelector('#author a');
-                link.text = music.title;
-                link.href = music.permalink_url;
+                link.text = music["title"];
+                link.href = music["permalink_url"];
+                
+                // $gd_music variables will let user render them in markdown comments
+                // <!-- $gd_music_download_url -->
 
                 audio.src = music.stream_url + '?client_id=' + client_id;
                 audio.play();
             }
         }
         var url = '//api.soundcloud.com/tracks/?';
-        url += 'q=' + query;
-        url += '&client_id=' + client_id;
+        //url += 'q=' + query + '&';
+        url += 'ids=' + '225718449' + '&';
+        url += 'client_id=' + client_id;
         http.open("GET", url, true);
         http.send();
     }
@@ -112,8 +125,33 @@ jQuery(document).ready(function() {
         // freq maxes at 256, scale freq to fit eq height
         var h = $('.eq').height();
         var f = 0 + (h - 0) * (freq - 0) / (0 - -256);
+        
+        // set band height
         $band.height(f);
+
+        // set band width
+        var width = $('.eq').width();
+        var w = ( width / bands ) - spacing;
+        $band.width(w);
+        
+        // set band's left position
+        $band.css( 'left', i * ( width / bands ) );
     }
 
+    function register_events() {
+        $(document).keyup(function(e) {
+            if( e.which == 37 || e.which == 100 ) {
+                var $prev = $('.toc a.current').prev()[0];
+                if (typeof $prev === "undefined") {
+                    $('.toc a:last-child')[0].click();
+                } else $prev.click();
+            } else if (e.keyCode === 39 || e.which == 102 ) {
+                var $next = $('.toc a.current').next()[0];
+                if (typeof $next === "undefined") {
+                    $('.toc a:first-child')[0].click();
+                } else $next.click();
+            }
+        })
+    }
 
 });
